c38789c3514a1a8280477acaf50796f6f1c66251
sbrotherton <password>
2021-02-19 15:14:51 +0000

    Correct android adaptive icon sizes and implement play store icon generation

--- src/modules/setIcon/android/config.ts
+++ src/modules/setIcon/android/config.ts
@@ -1,10 +1,9 @@
 export const config = {
-  androidIconSizes: [
-    { value: 36, density: 'ldpi' },
-    { value: 48, density: 'mdpi' },
-    { value: 72, density: 'hdpi' },
-    { value: 96, density: 'xhdpi' },
-    { value: 144, density: 'xxhdpi' },
-    { value: 192, density: 'xxxhdpi' },
-  ],
+    androidIconSizes: [
+        {value: 48, density: 'mdpi', adaptiveValue: 108},
+        {value: 72, density: 'hdpi', adaptiveValue: 162},
+        {value: 96, density: 'xhdpi', adaptiveValue: 216},
+        {value: 144, density: 'xxhdpi', adaptiveValue: 324},
+        {value: 192, density: 'xxxhdpi', adaptiveValue: 432},
+    ],
 };
--- src/services/image.processing.ts
+++ src/services/image.processing.ts
@@ -1,41 +1,66 @@
-import { normalize } from 'path';
-import sharp, { ResizeOptions } from 'sharp';
-import { createDirectoryIfNotExists } from './file.processing';
+import {normalize} from 'path';
+import sharp, {ResizeOptions} from 'sharp';
+import {createDirectoryIfNotExists} from './file.processing';
 
 export const generateResizedAssets = async (
-  sourcePath: string,
-  destinationPath: string,
-  width: number,
-  height: number = width,
-  options: ResizeOptions = {
-    fit: 'contain',
-  }
+    sourcePath: string,
+    destinationPath: string,
+    width: number,
+    height: number = width,
+    options: ResizeOptions = {
+        fit: 'contain',
+    }
 ) => {
-  createDirectoryIfNotExists(destinationPath);
-  return sharp(normalize(sourcePath))
-    .resize(width, height, options)
-    .toFile(destinationPath);
+    createDirectoryIfNotExists(destinationPath);
+    return sharp(normalize(sourcePath))
+        .resize(width, height, options)
+        .toFile(destinationPath);
+};
+
+export const generateAdaptiveAssets = async (
+    sourcePath: string,
+    destinationPath: string,
+    width: number,
+    height: number = width,
+    options: ResizeOptions = {
+        fit: 'contain',
+    }
+) => {
+    createDirectoryIfNotExists(destinationPath);
+    return sharp({
+        create: {
+            width,
+            height,
+            channels: 4,
+            background: "rgba(255, 255, 255, 0)"
+        }
+    }).composite([{
+        input: await sharp(normalize(sourcePath))
+            .resize(Math.round(width * 0.66), Math.round(height * 0.66), options)
+            .toBuffer()
+    }])
+        .toFile(destinationPath);
 };
 
 export const generateResizedAssetsWithoutAlpha = async (
-  sourcePath: string,
-  destinationPath: string,
-  width: number,
-  height: number = width,
-  options: ResizeOptions = {
-    fit: 'contain',
-  }
+    sourcePath: string,
+    destinationPath: string,
+    width: number,
+    height: number = width,
+    options: ResizeOptions = {
+        fit: 'contain',
+    }
 ) => {
-  createDirectoryIfNotExists(destinationPath);
-  return sharp(normalize(sourcePath))
-    .resize(width, height, options)
-    .removeAlpha()
-    .toFile(destinationPath);
+    createDirectoryIfNotExists(destinationPath);
+    return sharp(normalize(sourcePath))
+        .resize(width, height, options)
+        .removeAlpha()
+        .toFile(destinationPath);
 };
 
 export const checkImageIsSquare = async (sourcePath: string) => {
-  const { width, height } = await sharp(normalize(sourcePath)).metadata();
-  if (width !== height) {
-    throw new Error('Image is not squared');
-  }
+    const {width, height} = await sharp(normalize(sourcePath)).metadata();
+    if (width !== height) {
+        throw new Error('Image is not squared');
+    }
 };
